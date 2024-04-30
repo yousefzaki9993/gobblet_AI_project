103ae8de86977675425b9a0f1f43d91b16a52c90
Khaled Elsaeed <khaledawad552002@gmail.com>
2024-04-30 21:32:26 +0300

    Update BoardNode.java

--- src/gobblet/BoardNode.java
+++ src/gobblet/BoardNode.java
@@ -1,79 +1,92 @@
-package boardnode;
+package gobblet;
 
 import java.util.List;
 import java.util.ArrayList;
 
 
 public class BoardNode {
-    private boolean maxPlayer;
-    private int score;
-    private List<BoardNode> children;
-    private Board board;  // Board class used as an attribute
+private boolean maxPlayer;
+private int score;
+private List<BoardNode> children;
+private Board board; // Board class used as an attribute
+public int PrevX;
+public int PrevY;
+public Piece PrevPiece;
 
-    //constuctor
-    public BoardNode(Board board) {
-        this.board = board;
-    }
-
-    // Getters and setters for other attributes
-
-    public Board getBoard() {
-        return board;
-    }
-
-    public void setBoard(Board board) {
-        this.board = board;
-    }
-    
-    // methods 
-    
-    public List<BoardNode> getChildren (){
-    List<BoardNode> childrenCopy = new ArrayList <> (children); // To prevent external modification 
-        return childrenCopy;
-    }
-    
-    public void addChild (BoardNode child){
-        //check if there is no children and create list of children 
-        if (children == null){
-        children = new ArrayList<>(); 
-        }
-        children.add (child);
-    }
-    
-    public void evaluate (int depth, int scoreEval){
-        
-    }
-    
-    public int getScore (){
-        return score;
-    }
-    
-    public void setMaximizer (){
-        maxPlayer = true;
-        
-        if (children != null){
-        for (BoardNode child : children){
-            child.setMinimizer ();
-        }
-      }
-    }
-    
-    public void setMinimizer (){
-        maxPlayer = false;
-        
-        if (children != null){
-        for (BoardNode child : children){
-            child.setMaximizer ();
-        }
-      }
-    }
-    
-    public boolean isMaxPlayer (){
-        return maxPlayer ;
-    }
- 
-    
+//constuctor
+public BoardNode(Board board,Piece PrevPiece,int PrevX,int PrevY) {
+this.board = board;
+this.PrevPiece = PrevPiece;
+this.PrevX = PrevX;
+this.PrevY = PrevY;
 }
 
 
-    
+public void deleteBoard(){
+board = null;
+}
+// Getters and setters for other attributes
+
+public Board getBoard() {
+return board;
+}
+
+public void setBoard(Board board) {
+this.board = board;
+}
+
+// methods
+
+public List<BoardNode> getChildren (){
+List<BoardNode> childrenCopy = new ArrayList<>();
+if(children!= null) {
+childrenCopy = new ArrayList <> (children); // To prevent external modification
+}
+
+return childrenCopy;
+}
+
+public void addChild (BoardNode child){
+//check if there is no children and create list of children
+if (children == null){
+children = new ArrayList<>();
+}
+children.add (child);
+}
+
+
+public int getScore (){
+return score;
+}
+
+public void setMaximizer (){
+maxPlayer = true;
+
+if (children != null){
+for (BoardNode child : children){
+child.setMinimizer ();
+}
+}
+}
+
+public void setMinimizer (){
+maxPlayer = false;
+
+if (children != null){
+for (BoardNode child : children){
+child.setMaximizer ();
+}
+}
+}
+
+public boolean isMaxPlayer (){
+return maxPlayer ;
+}
+
+
+public void setScore(int score) { //Temporary function for testing
+this.score = score;
+}
+
+
+}
