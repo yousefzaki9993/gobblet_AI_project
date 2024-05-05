1e0b115917eaf55906c34728a679de5c2b7fe41e
Ahmed Salah <2100669@eng.asu.edu.eg>
2024-05-06 01:44:39 +0300

    deleted evaluation lines to move it to piece

--- src/gobblet/BoardNode.java
+++ src/gobblet/BoardNode.java
@@ -2,91 +2,195 @@ package gobblet;
 
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
+public class BoardNode implements Comparable<BoardNode> {
 
-public class BoardNode {
-private boolean maxPlayer;
-private int score;
-private List<BoardNode> children;
-private Board board; // Board class used as an attribute
-public int PrevX;
-public int PrevY;
-public Piece PrevPiece;
+    private boolean maxPlayer;
+    private int score;
+    private List<BoardNode> children = null;
+    private Board board;  // Board class used as an attribute
+    private int PrevX;
+    private int PrevY;
+    private Piece PrevPiece;
 
-//constuctor
-public BoardNode(Board board,Piece PrevPiece,int PrevX,int PrevY) {
-this.board = board;
-this.PrevPiece = PrevPiece;
-this.PrevX = PrevX;
-this.PrevY = PrevY;
-}
+    //constuctor
+    public BoardNode(Board board, Piece PrevPiece, int PrevX, int PrevY) {
+        this.board = board;
+        this.PrevPiece = PrevPiece;
+        this.PrevX = PrevX;
+        this.PrevY = PrevY;
+    }
 
+    public void deleteBoard() {
+        board = null;
+    }
+    // Getters and setters for other attributes
 
-public void deleteBoard(){
-board = null;
-}
-// Getters and setters for other attributes
+    public Board getBoard() {
+        return board;
+    }
 
-public Board getBoard() {
-return board;
-}
+    public void setBoard(Board board) {
+        this.board = board;
+    }
 
-public void setBoard(Board board) {
-this.board = board;
-}
+    // methods 
+    public List<BoardNode> getChildren() {
+//		List<BoardNode> childrenCopy = new ArrayList<>();
+//    	if(children!= null) {
+//    	    childrenCopy = new ArrayList <> (children); // To prevent external modification 
+//    	}
 
-// methods
+        return children;
+    }
 
-public List<BoardNode> getChildren (){
-List<BoardNode> childrenCopy = new ArrayList<>();
-if(children!= null) {
-childrenCopy = new ArrayList <> (children); // To prevent external modification
-}
+    public void addChild(BoardNode child) {
+        //check if there is no children and create list of children 
+        if (children == null) {
+            children = new ArrayList<>();
+        }
+        children.add(child);
+    }
 
-return childrenCopy;
-}
+    public void evaluate(int depth, ScoreEval ev) {
+        if (ev == null) {
+            throw new IllegalArgumentException("invalide evaloator");
+        }
+        if (depth < 0) {
+            throw new IllegalArgumentException("depth must be >= 0");
+        }
+        if (score == 10000 || score == -10000) {
+            return;
+        }
+        if (depth == 0) {
+            this.score = ev.evaluateBoard(board);
+            return;
+        }
+        if (board == null) {
+            throw new IllegalStateException("board missing!");
+        }
+        if (children == null) {
+            generateChildren();
+        }
+        for (BoardNode child : children) {
+            child.evaluate(depth - 1, ev);
+        }
+        if (!maxPlayer) {
+            children.sort((o1, o2) -> {
+                return o1.compareTo(o2);
+            });
+        } else {
+            children.sort((o1, o2) -> {
+                return -(o1.compareTo(o2));
+            });
+        }
+        this.score = children.get(0).getScore();
+    }
 
-public void addChild (BoardNode child){
-//check if there is no children and create list of children
-if (children == null){
-children = new ArrayList<>();
-}
-children.add (child);
-}
+    public int getScore() {
+        return score;
+    }
 
+    public void setMaximizer() {
+        maxPlayer = true;
 
-public int getScore (){
-return score;
-}
+        if (children != null) {
+            for (BoardNode child : children) {
+                child.setMinimizer();
+            }
+        }
+    }
 
-public void setMaximizer (){
-maxPlayer = true;
+    public void setMinimizer() {
+        maxPlayer = false;
 
-if (children != null){
-for (BoardNode child : children){
-child.setMinimizer ();
-}
-}
-}
+        if (children != null) {
+            for (BoardNode child : children) {
+                child.setMaximizer();
+            }
+        }
+    }
 
-public void setMinimizer (){
-maxPlayer = false;
+    public boolean isMaxPlayer() {
+        return maxPlayer;
+    }
 
-if (children != null){
-for (BoardNode child : children){
-child.setMaximizer ();
-}
-}
-}
+    public void setScore(int score) {      //Temporary function for testing
+        this.score = score;
+    }
 
-public boolean isMaxPlayer (){
-return maxPlayer ;
-}
+    @Override
+    public int compareTo(BoardNode node) {
+        return Integer.compare(this.score, node.score);
+    }
 
+    private void generateChildren() {
+        if (children != null) {
+            throw new IllegalStateException("node already has children");
+        }
+        if (board == null) {
+            throw new IllegalStateException("board not found");
+        }
+        try {
+            List<Piece> movables = board.getMovables(); //gets list of pieces that can be moved legally
+            for (int i = 0; i < movables.size(); i++) {
+                Piece piece = movables.get(i);
+                if ((!maxPlayer && piece.isBlack()) || (maxPlayer && !piece.isBlack())) { //if the piece fits the right turn
+                    for (int j = 0; j < 4; j++) { //iterate through every slot in the board
+                        for (int k = 0; k < 4; k++) {
 
-public void setScore(int score) { //Temporary function for testing
-this.score = score;
-}
+                            if (board.isLegal(piece, j, k)) {
+                                Board cloneBoard;
 
+                                cloneBoard = board.clone();
+
+                                //Make a move in the cloned board
+                                cloneBoard.move(cloneBoard.getPiece(piece.getX(), piece.getY()), j, k);
+
+                                //Add the cloned board to a board node
+                                BoardNode boardnode = new BoardNode(cloneBoard, piece, j, k);
+                                //set maximizer or minimizer
+                                if (maxPlayer) {
+                                    boardnode.setMinimizer();
+                                } else {
+                                    boardnode.setMaximizer();
+                                }
+                                //add the board node to the root node
+                                this.addChild(boardnode);
+                                //if a board node satisfy winning condition, make it a leaf node and give it max/min score
+                                Piece[] line = boardnode.getBoard().getWinningLine();
+                                if (line != null) {
+
+                                    int score = line[0].isBlack() ? -10000 : 10000;
+                                    boardnode.setScore(score);
+                                    boardnode.deleteBoard();
+                                }
+
+                            }
+                        }
+                    }
+                }
+            }
+        } catch (CloneNotSupportedException ex) {
+            Logger.getLogger(BoardNode.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        this.deleteBoard(); //setting board to null after generating children to save memory
+    }
+
+    public int getPrevX() {
+        return PrevX;
+    }
+
+    public int getPrevY() {
+        return PrevY;
+    }
+
+    public Piece getPrevPiece() {
+        return PrevPiece;
+    }
 
 }
--- src/gobblet/BoardTree.java
+++ src/gobblet/BoardTree.java
@@ -1,208 +1,52 @@
 package gobblet;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-import java.util.NoSuchElementException;
-/**
- * Represents a tree structure to explore possible moves on a game board with Alpha-Beta Pruning.
- */
 public class BoardTree {
-// Fields
 
-    /**
-     * The root node of the board tree.
-     */
-    public BoardNode rootNode;
-        /**
-     * The destination X coordinate for a move.
-     */
-    public int destX;
-       /**
-     * The destination Y coordinate for a move.
-     */
-    public int destY;
+    private final BoardNode root;
+    private final ScoreEval ev;
+    private int destX;
+    private int destY;
+    private Piece PieceToMove;
+    private int bestScore;
 
-    /**
-     * The piece to be moved in a particular move.
-     */
-    public Piece PieceToMove;
-       /**
-     * The score of the move.
-     */
-    public int moveScore;
-        /**
-     * Flag indicating whether to delete boards after generating the tree.
-     */
-    public boolean deleteBoards = true;
-    /**
-     * score evaluator object.
-     */
-    public ScoreEval ev;
-    // Constructor
-
-    /**
-     * Initializes a new instance of the BoardTree class.
-     *
-     * @param rootNode The root node of the board tree.
-     */
-    public BoardTree(BoardNode rootNode, ScoreEval ev) {
-        this.rootNode = rootNode;
+    public BoardTree(Board board, ScoreEval ev, boolean isBlackTurn) {
+        this.root = new BoardNode(board, null, 0, 0);
+        if (isBlackTurn) {
+            this.root.setMinimizer();
+        } else {
+            this.root.setMaximizer();
+        }
         this.ev = ev;
     }
-        // Public Methods
 
-    /**
-     * Gets the root node of the board tree.
-     *
-     * @return The root node of the board tree.
-     */
-    public BoardNode getValue() {
-        return rootNode;
-    }
-        /**
-     * Generates a tree structure exploring possible moves on the board up to a specified depth.
-     *
-     * @param depth             The depth to explore in the tree.
-     * @param originalRootNode  The original root node before any modifications.
-     * @throws CloneNotSupportedException Thrown if cloning of the board is not supported.
-     */
-    public void generateTree(int depth, BoardNode originalRootNode) throws CloneNotSupportedException { //function for generating tree of possible moves
-        if (depth == 0) {
-            originalRootNode.setScore(ev.evaluateBoard(originalRootNode.getBoard()));
-            if(deleteBoards)rootNode.deleteBoard();
-            return;
-        }
+    public void evaluate(int depth) {
 
-        Board board = rootNode.getBoard();
-        List<Piece> movables = board.getMovables(); //gets list of pieces that can be moved legally
+        root.evaluate(depth, ev, null, null, Integer.MAX_VALUE);
+        destX = root.getChildren().get(0).getPrevX();
+        destY = root.getChildren().get(0).getPrevY();
+        PieceToMove = root.getChildren().get(0).getPrevPiece();
+        System.out.println("");
+        for (BoardNode q : root.getChildren()) {
+            System.out.print(", " + q.getScore());
+        }
+        System.out.println("");
+        bestScore = root.getScore();
+    }
 
-        for (int i = 0; i < movables.size(); i++) {
-            Piece piece = movables.get(i);
-            if ((!rootNode.isMaxPlayer() && piece.isBlack()) || (rootNode.isMaxPlayer() && !piece.isBlack())) { //if the piece fits the right turn
-                for (int j = 0; j < 4; j++) { //iterate through every slot in the board
-                    for (int k = 0; k < 4; k++) {
-                        if (board.isLegal(piece, j, k)) {
-                            Board cloneBoard = board.clone();
-                            //Make a move in the cloned board
-                            cloneBoard.move(cloneBoard.getPiece(piece.getX(), piece.getY()), j, k);
-                            //Add the cloned board to a board node
-                            BoardNode boardnode = new BoardNode(cloneBoard, piece, j, k);
-                            //set maximizer or minimizer
-                            if (rootNode.isMaxPlayer()) {
-                                boardnode.setMinimizer();
-                            } else {
-                                boardnode.setMaximizer();
-                            }
-                            //add the board node to the root node
-                            rootNode.addChild(boardnode);
-                            
-                            //if a board node satisfy winning condition, make it a leaf node and give it max/min score
-                            Piece[] line = boardnode.getBoard().getWinningLine();
-                            if(line != null) {
-                            	int score = line[0].isBlack() ? -10000:10000;
-                            	boardnode.setScore(score);
-                            	continue;
-                            }
-                            
-                            //change the rootNode for tree generation
-                            this.rootNode = boardnode;
-                            this.generateTree(depth - 1, boardnode);
-                            
-                            if(deleteBoards)rootNode.deleteBoard(); //setting board to null after generating children to save memory
-                            this.rootNode = originalRootNode;
-                        }
-                    }
-                }
-            }
-        }
+    public int getBestScore() {
+        return bestScore;
     }
-        /**
-     * Applies Alpha-Beta Pruning to find the best move and updates destination coordinates and piece to move.
-     */
-    public void AlphaBetaPruning() {
-    	
-        moveScore = alphaBeta(rootNode,-1000000,1000000,rootNode.isMaxPlayer()); 
-        BoardNode MinMaxNode;
-        if (rootNode.isMaxPlayer()) { //returns the node that represents the best next move
-            MinMaxNode = getMaxScoreNode(rootNode.getChildren());
-        } else {
-            MinMaxNode = getMinScoreNode(rootNode.getChildren());
-        }
-        //sets variables for what piece should be moved to what x and y
-        this.destX = MinMaxNode.PrevX;
-        this.destY = MinMaxNode.PrevY;
-        this.PieceToMove = MinMaxNode.PrevPiece;
-    }
-        // Static Methods
 
-    /**
-     * Applies Alpha-Beta Pruning algorithm to evaluate the best move score.
-     *
-     * @param node              The current node in the tree.
-     * @param alpha             The best score that the maximizing player is assured of.
-     * @param beta              The best score that the minimizing player is assured of.
-     * @param maximizingPlayer True if the current player is maximizing, false otherwise.
-     * @return The best move score.
-     */
-    static int alphaBeta(BoardNode node,int alpha, int beta, boolean maximizingPlayer) {
-    	
-        if (node.getChildren().isEmpty()) { // base condition that checks if node is a leaf node
-        	 return node.getScore();
-        }
-           
-        if (maximizingPlayer) {
-            int maxEval = Integer.MIN_VALUE;
-            for (BoardNode child : node.getChildren()) {
-                int eval = alphaBeta(child, alpha, beta, false);
-                maxEval = Math.max(maxEval, eval);
-                
-                alpha = Math.max(alpha, eval);
-                
-                if (beta <= alpha) {
-                    break;
-                }
-                
-            }
-            node.setScore(maxEval);
-            return maxEval;
-        } else {
-            int minEval = Integer.MAX_VALUE;
-            for (BoardNode child : node.getChildren()) {
-                int eval = alphaBeta(child, alpha, beta, true);
-                minEval = Math.min(minEval, eval);
-                beta = Math.min(beta, eval);
-                if (beta <= alpha) {
-                    break;
-                }
-            }
-            node.setScore(minEval);
-            return minEval;
-        }
+    public int getDestX() {
+        return destX;
     }
- 
-    // Helper methods
 
-    /**
-     * Gets the board node with the minimum score from a list of board nodes.
-     *
-     * @param childrenList The list of board nodes.
-     * @return The board node with the minimum score.
-     */
-    public BoardNode getMinScoreNode(List<BoardNode> childrenList) { //return the node with minimum score from a list of children nodes
-        BoardNode tempNode = childrenList.stream().min(Comparator.comparingInt(BoardNode::getScore)).orElseThrow(NoSuchElementException::new);
-        return tempNode;
+    public int getDestY() {
+        return destY;
     }
-        /**
-     * Gets the board node with the maximum score from a list of board nodes.
-     *
-     * @param childrenList The list of board nodes.
-     * @return The board node with the maximum score.
-     */
-    public BoardNode getMaxScoreNode(List<BoardNode> childrenList) {//return the node with maximum score from a list of children nodes
-        BoardNode tempNode = childrenList.stream().max(Comparator.comparingInt(BoardNode::getScore)).orElseThrow(NoSuchElementException::new);
-        return tempNode;
+
+    public Piece getPieceToMove() {
+        return PieceToMove;
     }
     
-
 }
