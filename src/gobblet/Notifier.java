6c5111aab088f6ffa643b31bdc473bbe0628c73a
Ahmed Salah <2100669@eng.asu.edu.eg>
2024-05-06 01:48:17 +0300

    solved AI not pausing bug

--- src/gobblet/Notifier.java
+++ src/gobblet/Notifier.java
@@ -1,31 +1,67 @@
-
 package gobblet;
 
 import java.util.ArrayList;
 import java.util.List;
 
-
 public class Notifier {
+    private boolean paused=false;
+    boolean nextPlayer;
     private List<Observer> observers = new ArrayList<>();
-    public void add(Observer o){
+    
+    public void pause(){
+        paused = true;
+    }
+    public void resume(){
+        paused = false;
+        notifyPlayer(nextPlayer);
+    }
+    
+    public void add(Observer o) {
         observers.add(o);
     }
-    public boolean remove(Observer o){
+    
+    public boolean remove(Observer o) {
         return observers.remove(o);
     }
-    public void notifyPlayersEnd(){
-        for(Observer o : observers){
+
+    public void notifyPlayersEnd() {
+        for (Observer o : observers) {
             o.endGame();
         }
     }
-    public void notifyPlayer(boolean isBlack){
-        for(Observer o : observers){
-            if(!o.isNeutral() && o.isBlack() == isBlack)o.startRole();
+
+    public void notifyPlayer(boolean isBlack) {
+        if(paused){
+            nextPlayer = isBlack;
+            return;
+        }
+//        Thread t = new Thread(() -> {
+            for (Observer o : observers) {
+                o.switchRole();
+            }
+//        });
+//        t.start();
+        for (Observer o : observers) {
+
+            if (!o.isNeutral() && o.isBlack() == isBlack) {
+                if (o instanceof AIPlayer) {
+                    Thread t1 = new Thread(() -> {
+                        o.startRole();
+                    });
+                    t1.start();
+                } else {
+                    o.startRole();
+                }
+
+            }
+
         }
     }
 
     public void addNeutralObserver(Observer o) {
-        if(!o.isNeutral())throw new IllegalArgumentException("non-playing observers must be neutrals!!");
+        if (!o.isNeutral()) {
+            throw new IllegalArgumentException("non-playing observers must be neutrals!!");
+        }
         observers.add(o);
     }
 }
